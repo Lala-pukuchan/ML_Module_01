a8881d17ae3bdf3fa8c2b751ead7638ccd015b6c
小林瑠理 <kobayashi@kobayashis-MacBook-Pro.local>
2023-12-07 20:39:58 +0100

    ex02, 03

---
+++ ex03/my_linear_regression.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+
+class MyLinearRegression:
+    """
+    Description:
+        My personal linear regression class to fit like a boss.
+    """
+    def __init__(self, thetas, alpha=0.001, max_iter=1000):
+        self.alpha = alpha
+        self.max_iter = max_iter
+        self.thetas = thetas
+
+    def gradient(self, x, y):
+        if (
+            x.size == 0
+            or y.size == 0
+            or self.thetas.size == 0
+            or x.shape[0] != y.shape[0]
+            or self.thetas.shape != (2, 1)
+        ):
+            return None
+        m = x.shape[0]
+        x_b = np.hstack((np.ones((m, 1)), x))
+        gradient = (1 / m) * x_b.T.dot(x_b.dot(self.thetas) - y)
+
+        return gradient
+
+    def fit_(self, x, y):
+        if x.shape[0] != y.shape[0] or self.thetas.shape != (2, 1):
+            return None
+
+        for _ in range(self.max_iter):
+            grad = self.gradient(x, y)
+            if grad is None:
+                return None
+            self.thetas = self.thetas - self.alpha * grad
+
+        return self.thetas
+
+    def predict_(self, x):
+        if (not isinstance(x, np.ndarray)
+                or not isinstance(self.thetas, np.ndarray)):
+            return None
+        if x.size == 0 or self.thetas.size == 0:
+            return None
+        if self.thetas.shape != (2, 1):
+            return None
+        y_hat = self.thetas[0] + self.thetas[1] * x
+        return y_hat
+
+    def loss_elem_(self, y, y_hat):
+        if y.shape != y_hat.shape:
+            return None
+        return (y_hat - y) ** 2
+
+    def loss_(self, y, y_hat):
+        if y.shape != y_hat.shape:
+            return None
+        m = y.shape[0]
+        return (1 / (2 * m)) * np.sum((y_hat - y) ** 2)
+
+
+print("Example 1:")
+x = np.array([
+        [12.4956442], [21.5007972], [31.5527382], [48.9145838], [57.5088733]
+        ])
+y = np.array([
+        [37.4013816], [36.1473236], [45.7655287], [46.6793434], [59.5585554]
+        ])
+lr1 = MyLinearRegression(np.array([[2], [0.7]]))
+y_hat = lr1.predict_(x)
+print(lr1.predict_(x))
+print(lr1.loss_(y, y_hat))
+
+print("Example 2:")
+lr2 = MyLinearRegression(np.array([[1], [1]]), 5e-8, 1500000)
+lr2.fit_(x, y)
+print(lr2.thetas)
+y_hat = lr2.predict_(x)
+print(y_hat)
+print(lr2.loss_elem_(y, y_hat))
+print(lr2.loss_(y, y_hat))
