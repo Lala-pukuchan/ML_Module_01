0979d001d84fefa36eb6b7639955920beb70fa0e
Lala_pukuchan <ruruover1105@gmail.com>
2023-12-19 09:41:07 +0100

    ex06

--- ex06/minmax.py
+++ ex06/minmax.py
@@ -28,8 +28,24 @@ def minmax(x):
 output_file = "results/ex06/result_ex06.txt"
 
 with open(output_file, "w") as file:
-
+    print("--- minmax X ---", file=file)
     X = np.array([0, 15, -9, 7, 12, 3, -21]).reshape((-1, 1))
     print(minmax(X), file=file)
+    print(
+        "expected       :\n",
+        np.array(
+            [0.58333333, 1.0, 0.33333333, 0.77777778, 0.91666667, 0.66666667, 0.0]
+        ),
+        file=file,
+    )
+
+    print("--- minmax Y ---\n", file=file)
     Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
     print(minmax(Y), file=file)
+    print(
+        "expected       :\n",
+        np.array(
+            [0.63636364, 1.0, 0.18181818, 0.72727273, 0.93939394, 0.6969697, 0.0]
+        ),
+        file=file,
+    )
