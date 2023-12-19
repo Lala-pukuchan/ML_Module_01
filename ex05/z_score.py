fa1e616790a7f31c64d59c4edbd5ebb0e04cd356
Lala_pukuchan <ruruover1105@gmail.com>
2023-12-19 09:37:27 +0100

    ex05

--- ex05/z_score.py
+++ ex05/z_score.py
@@ -22,8 +22,40 @@ def zscore(x):
 output_file = "results/ex05/result_ex05.txt"
 
 with open(output_file, "w") as file:
-
+    print("--- zscore X ---", file=file)
     X = np.array([0, 15, -9, 7, 12, 3, -21])
     print(zscore(X), file=file)
+    print(
+        "expected       :\n",
+        np.array(
+            [
+                -0.08620324,
+                1.2068453,
+                -0.86203236,
+                0.51721942,
+                0.94823559,
+                0.17240647,
+                -1.89647119,
+            ]
+        ),
+        file=file,
+    )
+
+    print("--- zscore Y ---", file=file)
     Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
     print(zscore(Y), file=file)
+    print(
+        "expected       :\n",
+        np.array(
+            [
+                0.11267619,
+                1.16432067,
+                -1.20187941,
+                0.37558731,
+                0.98904659,
+                0.28795027,
+                -1.72770165,
+            ]
+        ),
+        file=file,
+    )
