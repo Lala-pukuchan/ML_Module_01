abc80a9d8cc643a9c9494329a95f295486d60a95
Lala_pukuchan <ruruover1105@gmail.com>
2023-12-19 08:32:16 +0100

    ex01

--- ex01/vec_gradient.py
+++ ex01/vec_gradient.py
@@ -45,15 +45,20 @@ with open(output_file, "w") as file:
     x = np.array(
         [12.4956442, 21.5007972, 31.5527382, 48.9145838, 57.5088733]
         ).reshape(
-        (-1, 1)
-    )
+            (-1, 1)
+        )
     y = np.array(
         [37.4013816, 36.1473236, 45.7655287, 46.6793434, 59.5585554]
         ).reshape(
         (-1, 1)
     )
 
+    print("---test.1---", file=file)
     theta1 = np.array([2, 0.7]).reshape((-1, 1))
-    print(gradient(x, y, theta1), file=file)
+    print("gradient with theta1: \n", gradient(x, y, theta1), file=file)
+    print("expected            : \n", np.array([[-19.0342574], [-586.66875564]]), file=file)
+
+    print("\n---test.2---", file=file)
     theta2 = np.array([1, -0.4]).reshape((-1, 1))
-    print(gradient(x, y, theta2), file=file)
+    print("gradient with theta2: \n", gradient(x, y, theta2), file=file)
+    print("expected            : \n", np.array([[-57.86823748], [-2230.12297889]]), file=file)
